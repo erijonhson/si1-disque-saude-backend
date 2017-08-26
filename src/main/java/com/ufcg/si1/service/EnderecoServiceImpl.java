eb5eb9a32bee889c1260b61a51fca76ac1c0d811
erijonhson <eri.silva@ccc.ufcg.edu.br>
2017-08-26 01:28:10 -0300

    Teste endereço.

--- src/main/java/com/ufcg/si1/service/EnderecoServiceImpl.java
+++ src/main/java/com/ufcg/si1/service/EnderecoServiceImpl.java
@@ -2,6 +2,8 @@ package com.ufcg.si1.service;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +13,7 @@ import com.ufcg.si1.repository.EnderecoRepository;
 @Service(value = "enderecoService")
 public class EnderecoServiceImpl implements EnderecoService {
 
-	@Autowired
+	@Resource(name = "enderecoRepository")
 	EnderecoRepository enderecoRepository;
 
 	@Override
